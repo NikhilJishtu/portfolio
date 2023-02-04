173533dfaef2f194082eb408a87d8304a7bc7fbb
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2023-02-04 19:26:53 +0100

    contact

---
+++ Website/index/contact.php
@@ -0,0 +1,44 @@
+<?php
+
+$message_sent = false;
+if (isset($_POST['email']) && $_POST['email'] != '') {
+
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+    // verstuur het formulier
+    $userName = $_POST['name'];
+    $userEmail = $_POST['email'];
+    $messageSubject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "blah@blah.com";
+    $body = "";
+
+    $body .= "From: " . $userName . "\r\n";
+    $body .= "Email: " . $userEmail . "\r\n";
+    $body .= "Message: " . $message . "\r\n";
+
+    // mail($to, $messageSubject, $body);
+
+    $message_sent = true;
+    } 
+
+    else {
+        $invalid_class_name = "form-invalid";
+    }
+}
+?>
+
+<?php
+if($message_sent):
+?>
+
+<h3>Thanks, I'll be in touch!</h3>
+
+<?php
+else:
+?>
+
+<?php
+endif;
+?>
--- Website/index/script3.js
+++ Website/index/script3.js
@@ -1,51 +1,15 @@
-function animateFrom3( elem, direction ) {
-    // eslint-disable-next-line no-bitwise
-    direction = direction | 20;
+$(function() {
+   
+    $(".form-control").on('focus', function(){
 
-    let x = direction * 100;
-    let y = 0;
+        $(this).parents(".form-group").addClass('focused');
 
-    if ( elem.classList.contains( 'text-reveal3-left' ) ) {
-        x = 0;
-        y = -100;
-    } else if ( elem.classList.contains( 'text-reveal3-right' ) ) {
-        x = 0;
-        y = 100;
-    }
+    });
 
-    gsap.fromTo( elem, {
-        x: x,
-        y: y,
-        autoAlpha: 0,
-    }, {
-        duration: 1,
-        x: 0,
-        y: 0,
-        autoAlpha: 1,
-        overwrite: 'auto',
-        force3D: true,
-    } );
-}
+    $(".form-control").on('blur', function(){
 
-function hide( elem ) {
-    gsap.killTweensOf( elem );
-    gsap.set( elem, {autoAlpha: 0 } );
-}
+        $(this).parents(".form-group").removeClass('focused');
 
-function hideKill( elem ) {
-    gsap.killTweensOf( elem );
-    gsap.to( elem, { duration: 1.5, autoAlpha: 0, y: 100 } );
-}
-// start scroll trigger animations
-const $elemsToReveal3 = document.querySelectorAll( '.text-reveal3' );
+    });
 
-gsap.utils.toArray( $elemsToReveal3 ).forEach( ( elem ) => {
-    hide( elem );
-    ScrollTrigger.create( {
-        trigger: elem,
-        onEnter: () => animateFrom3( elem ),
-        onEnterBack: () => animateFrom3( elem),
-        onLeave: () => hide( elem ),
-        onLeaveBack: () => hideKill( elem ),
-    } );
-} );
+});
