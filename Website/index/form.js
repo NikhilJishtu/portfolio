cc4d8fa77cef070d922495cc96a08e550d59cc90
GekkePerp <95859662+GekkePerp@users.noreply.github.com>
2023-02-05 17:03:31 +0100

    form

--- Website/index/form.js
+++ Website/index/form.js
@@ -13,3 +13,18 @@ $(function() {
     });
 
 });
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    fetch("contact.php", {
+      method: "POST",
+      body: formData
+    })
+    .then(response => response.text())
+    .then(result => {
+      // Do something with the result
+      window.alert("Thanks, I will be in touch!");
+    });
+  });
